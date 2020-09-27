125facfcf2a43dcfcfb754b69584929d8599d570
Lee Michaeli <leeisr@gmail.com>
2020-09-27 07:13:56 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ def join_nested_strings(src)
     while element_index < src[row_index].length do 
       if src[row_index][element_index].class == String 
         combined_string << src[row_index][element_index]
-      end
+
     element_index += 1 
   end
   end
